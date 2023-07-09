5974e1dcbf0ad8a33c25ac767eb015bcd6a374b0
SofiyaKH <119045257+SofiyaKH@users.noreply.github.com>
2023-07-09 11:11:05 +0300

    Подключение ORM. Разработка CRUD операций

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,33 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { CategoryModule } from './category/category.module';
+import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { Category } from './category/entities/category.entity';
+import { Post } from './posts/entities/post.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    CategoryModule,
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      database: process.env.DB_DBNAME,
+      entities: [Category, Post],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
